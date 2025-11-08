4c06a362230b9d0aa3867f0faa3a9d7cb421c6f5
BRIGHAM450 <brighamcarreraventura@gmail.com>
2025-11-08 18:35:41 -0500

    prueba 1

---
+++ scripts/getdates.js
@@ -0,0 +1,5 @@
+// Mostrar el año actual
+document.getElementById("currentyear").textContent = new Date().getFullYear();
+
+// Mostrar la última fecha de modificación del documento
+document.getElementById("lastModified").textContent = document.lastModified;
